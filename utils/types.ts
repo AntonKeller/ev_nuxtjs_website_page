dd77682b1e49cf6527a770873190e06d95a2c0c7
Anton Keller <ToniWebDev@mail.ru>
2023-02-13 14:19:43 +0300

    types was extended

--- utils/types.ts
+++ utils/types.ts
@@ -4,3 +4,20 @@ export interface link {
   url: string,
   transition?: string
 }
+
+export interface ratingButton {
+  id: Number,
+  title: String,
+  year: Number,
+}
+
+interface ratingKey {
+  title: String,
+  value: String,
+}
+
+export interface ratingDescription {
+  id: Number,
+  year: Number,
+  keys: Array<ratingKey>
+}
