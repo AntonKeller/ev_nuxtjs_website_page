a8b5f1e98985fc201643dbd413b775dbdf9e4b9b
Anton Keller <ToniWebDev@mail.ru>
2023-02-28 17:58:57 +0300

    added experience page

---
+++ configs/experience.ts
@@ -0,0 +1,60 @@
+interface Tab {
+  id: number,
+  value: string,
+  style: string,
+}
+
+export const tabs: Array<Tab> = [
+  {
+    id: 1,
+    value: "оценка бизнеса",
+    style: "tab-transition-5"
+  },
+  {
+    id: 2,
+    value: "оценка имущественных активов",
+    style: "tab-transition-5"
+  },
+  {
+    id: 3,
+    value: "оценка",
+    style: "tab-transition-5"
+  },
+  {
+    id: 4,
+    value: "строительный аудит",
+    style: "tab-transition-5"
+  },
+  {
+    id: 5,
+    value: "финансово-технический надзор",
+    style: "tab-transition-5"
+  },
+]
+
+
+interface imageLink {
+  id: number,
+  name: string,
+}
+
+export const logotypes: Array<imageLink> = [
+  {id: 1, name: "l1"},
+  {id: 2, name: "l2"},
+  {id: 3, name: "l3"},
+  {id: 4, name: "l4"},
+  {id: 5, name: "l5"},
+  {id: 6, name: "l6"},
+  {id: 7, name: "l7"},
+  {id: 8, name: "l8"},
+  {id: 9, name: "l9"},
+  {id: 10, name: "l10"},
+  {id: 11, name: "l11"},
+  {id: 12, name: "l12"},
+  {id: 13, name: "l13"},
+  {id: 14, name: "l14"},
+  {id: 15, name: "l15"},
+  {id: 16, name: "l16"},
+  {id: 17, name: "l17"},
+  {id: 18, name: "l18"}
+]
--- pages/experience/index.vue
+++ pages/experience/index.vue
@@ -1,20 +1,90 @@
 <template>
-  <div>
-    page experience
+  <div class="bg">
+    <div class="h-screen w-screen flex flex-col justify-center items-center">
+
+      <div
+        class="basis-3/12 w-10/12 font-bold hidden md:flex flex-row items-end text-gray-300 text-lg uppercase h-full md:pt-24">
+        <div
+          v-for="tab of tabs"
+          :key="tab.id"
+          :style="{animationDuration: 0.5 + tab.id / 10 + 's'}"
+          @click.left="activeTab = tab.id"
+          :class="{'backdrop-brightness-75': activeTab === tab.id}"
+          class="text-center border-t border-t-gray-600 lg:text-xl tab-transition duration-300 hover:backdrop-brightness-75 cursor-pointer border-b border-b-gray-600 p-6 w-full">
+          {{ tab.value }}
+        </div>
+      </div>
+
+      <div class="basis-9/12 w-10/12 h-screen flex flex-row flex-wrap justify-center items-center overflow-y-scroll md:pt-10">
+        <img
+          v-for="logotype of logotypes"
+          :key="logotype.id"
+          :style="{animationDuration: 0.7 + logotype.id / 10 + 's'}"
+          :src="require(`./../../assets/experience_logotypes/${logotype.name}.png`)"
+          class="hover:opacity-50 img-animate p-4 object-contain object-center"
+          alt="no image">
+      </div>
+
+    </div>
   </div>
+
 </template>
 
 <script>
-import evHeader from "@/components/widgets/ev-header.vue"
+
+import {tabs, logotypes} from "@/configs/experience"
 
 export default {
+
   name: "experience",
-  components: {
-    'experienceHeader': evHeader,
+
+  methods: {},
+
+  mounted() {
+    // setInterval(() => {
+    //   if (this.tabs.length === this.activeTab) this.activeTab = 0
+    //
+    //   this.activeTab += 1;
+    //
+    // }, 1000);
   },
+
+  data() {
+    return {
+      tabs: tabs,
+      logotypes: logotypes,
+      activeTab: 0,
+    }
+  }
+
 }
 </script>
 
 <style scoped>
 
+.tab-transition {
+  animation: show-tab ease;
+}
+
+@keyframes show-tab {
+  from {
+    opacity: 0;
+    transform: translateY(-100vh);
+  }
+}
+
+.bg {
+  background: url("assets/background/f-10.png") transparent no-repeat center / cover;
+}
+
+.img-animate {
+  animation: show ease;
+}
+
+@keyframes show {
+  from {
+    opacity: 0;
+    transform: translateX(100vw);
+  }
+}
 </style>
