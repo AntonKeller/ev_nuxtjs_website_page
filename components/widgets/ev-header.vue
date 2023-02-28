59c943566926c09efde5640cbd64ae1eb68a9456
Anton Keller <ToniWebDev@mail.ru>
2023-02-28 17:58:21 +0300

    header refactor

--- components/widgets/ev-header.vue
+++ components/widgets/ev-header.vue
@@ -1,37 +1,37 @@
 <template>
   <!--fixed top-0-->
-  <header class="flex flex-col items-stretch">
+  <header class="flex flex-col items-stretch z-50">
 
     <div
-      class="z-20 bg-transparent top-0 w-full flex items-center justify-between sm:justify-between md:justify-between border-b border-b-gray-700 p-1 sm:p-4 md:pl-11 md:pr-11 z-10 backdrop-blur-sm transition-all text-black md:absolute md:text-red-50">
+      class="z-20 bg-transparent top-0 w-full flex items-center justify-between sm:justify-between md:justify-between border-b border-b-gray-700 p-2 sm:p-4 md:pl-11 md:pr-11 z-10 backdrop-blur-sm transition-all text-black md:absolute md:text-red-50">
+
       <NuxtLink class="move mr-auto ml-1 sm:ml-5 flex justify-center items-center" to="/">
-        <img @click="pageisOpen=0" class="select-none w-15 h-8 sm:w-20 sm:h-11 md:w-24 md:h-14"
+        <img @click="activePage=0" class="select-none w-15 h-10 sm:w-20 sm:h-11 md:w-24 md:h-14"
              src="@/assets/images/logotype-white.png" alt="">
       </NuxtLink>
-
       <nav class="hidden sm:flex sm:items-center sm:justify-center md:pr-3 md:mr-3 mr-3">
-        <ul class="flex text-gray-200">
+        <ul class="flex text-gray-300">
           <li
-            @click="pageisOpen=link.id"
-            class="hover:text-gray-500 ml-6 md:ml-3 lg:ml-14 flex items-center transition-all ease-out cursor-pointer"
+            @click="activePage=link.id"
+            class="hover:text-gray-400 ml-6 md:ml-3 lg:ml-14 flex items-center transition-all ease-out cursor-pointer"
             v-for="link of navLinks">
-            <div :class="{'-rotate-90 text-red-200': pageisOpen === link.id}" class="hidden md:block text-center w-4 h-4 transition-all duration-150 ease-in-out"><</div>
-            <NuxtLink class="md:ml-4" :class="{'text-red-200': pageisOpen === link.id}" :to="link.url">
+            <div :class="{'-rotate-90': activePage === link.id}"
+                 class="hidden md:block text-center w-4 h-4 transition-all duration-150 ease-in-out"><
+            </div>
+            <NuxtLink class="md:ml-4" :to="link.url">
               <h5 class="sm:text-base md:text-lg lg:text-xl">{{ link.title }}</h5>
               <p class="hidden md:block sm:text-xs md:text-sm lg:text-base">{{ link.description }}</p>
             </NuxtLink>
           </li>
         </ul>
       </nav>
-
-      <svg class="hidden sm:block w-11 h-11 pl-2 cursor-pointer text-gray-300"
+      <svg class="sm:block w-10 h-10 pl-2 cursor-pointer text-gray-300"
            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
            stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round"
               d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
       </svg>
-
-      <div class="block sm:hidden flex items-center">
+      <div @click="menuIsOpen=!menuIsOpen" class="block sm:hidden flex items-center">
         <svg class=" cursor-pointer w-10 h-10 ml-4 text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="none"
              viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
@@ -39,6 +39,20 @@
       </div>
 
     </div>
+
+    <div v-if="menuIsOpen" class="a-menu sm:hidden bg-transparent pt-4 border-b border-b-gray-700">
+      <ul class="flex flex-col text-gray-300">
+        <li
+          @click="openPage(link.id)"
+          class="cursor-pointer p-2 pl-4"
+          v-for="link of navLinks">
+          <NuxtLink :to="link.url">
+            <h5 class="text-base">{{ link.title }}</h5>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+
   </header>
 </template>
 
@@ -52,10 +66,18 @@ export default {
     textColor: String
   },
 
+  methods: {
+    openPage(id) {
+      this.activePage = id;
+      this.menuIsOpen = false;
+    }
+  },
+
   data() {
     return {
       navLinks,
-      pageisOpen: 0,
+      activePage: 0,
+      menuIsOpen: false
     }
   },
 
@@ -63,5 +85,16 @@ export default {
 </script>
 
 <style>
+  .a-menu {
+    animation: drop-menu ease 0.4s
+  }
 
+  @keyframes drop-menu {
+    from {
+      transform: translateX(-100vw);
+      opacity: 0;
+    }
+  }
 </style>
+
+
